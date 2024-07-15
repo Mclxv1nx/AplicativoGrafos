a30e0fb4672c32831801f364cd459b8c1c7efbaa
Carlos Puentestar <puentestarchamorrocarlosdaniel@gmail.com>
2024-07-15 08:13:24 -0500

    Update Dijkstra.cs

---
+++ AplicativoGrafos/AplicativoGrafos/Dijkstra.cs
@@ -0,0 +1,93 @@
+using System;
+using System.Collections.Generic;
+
+namespace AplicativoGrafos
+{
+    internal class NodoD
+    {
+        public string Id { get; set; }
+        public List<AristaD> Aristas { get; set; } = new List<AristaD>();
+
+        public NodoD(string id)
+        {
+            Id = id;
+        }
+
+        public void AgregarArista(NodoD destino, int peso)
+        {
+            Aristas.Add(new AristaD(destino, peso));
+        }
+    }
+
+    internal class AristaD
+    {
+        public NodoD Destino { get; set; }
+        public int Peso { get; set; }
+
+        public AristaD(NodoD destino, int peso)
+        {
+            Destino = destino;
+            Peso = peso;
+        }
+    }
+
+    internal class Dijkstra
+    {
+        public Dictionary<string, NodoD> Nodos { get; set; } = new Dictionary<string, NodoD>();
+
+        public void AgregarNodo(string id)
+        {
+            Nodos[id] = new NodoD(id);
+        }
+
+        public void AgregarArista(string origenId, string destinoId, int peso)
+        {
+            NodoD origen = Nodos[origenId];
+            NodoD destino = Nodos[destinoId];
+            origen.AgregarArista(destino, peso);
+        }
+
+        public Dictionary<string, int> GDijkstra(string inicioId)
+        {
+            var distancias = new Dictionary<string, int>();
+            var nodoPredecesor = new Dictionary<string, string>();
+            var nodosSinVisitar = new HashSet<string>();
+
+            foreach (var nodo in Nodos.Keys)
+            {
+                distancias[nodo] = int.MaxValue / 2;
+                nodosSinVisitar.Add(nodo);
+            }
+
+            distancias[inicioId] = 0;
+
+            while (nodosSinVisitar.Count > 0)
+            {
+                string nodoActual = null;
+
+                foreach (var nodo in nodosSinVisitar)
+                {
+                    if (nodoActual == null || distancias[nodo] < distancias[nodoActual])
+                    {
+                        nodoActual = nodo;
+                    }
+                }
+
+                nodosSinVisitar.Remove(nodoActual);
+
+                foreach (var arista in Nodos[nodoActual].Aristas)
+                {
+                    int nuevaDistancia = distancias[nodoActual] + arista.Peso;
+
+                    if (nuevaDistancia < distancias[arista.Destino.Id])
+                    {
+                        distancias[arista.Destino.Id] = nuevaDistancia;
+                        nodoPredecesor[arista.Destino.Id] = nodoActual;
+                    }
+                }
+            }
+
+            return distancias;
+        }
+    }
+}
